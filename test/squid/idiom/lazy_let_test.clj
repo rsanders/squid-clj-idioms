a3e14bfd95fa78565e3dfa3b811f42c1ccb91c74
Robert Sanders <robert@zeevex.com>
2012-05-07 21:41:09 -0400

    explanation of atom / counter test

--- test/squid/idiom/lazy_let_test.clj
+++ test/squid/idiom/lazy_let_test.clj
@@ -14,6 +14,17 @@
     ))
 
 ;; this uses an atom to count side-effects
+;;
+;; explanation of final vector in the result list:
+;;
+;;   first ref to atomval2 should resolve (and cache) base atom val of 0
+;;   any use of @counter will return current atom val, at this point
+;;       in time still 0
+;;   first ref of atomval1 will inc counter and eval to 1
+;;   second and subsequent refs of atomval1 will return cached value of 1
+;;   next ref of @counter returns current counter val of 1
+;;   atomval2 should still return cached value of0
+;;
 (deftest lazy-let-destructuring
   (binding [counter (atom 0)]
     (testing "lazy-let with destructuring and simple backref"
@@ -24,8 +35,8 @@
                       atomval1 (swap! counter inc)
                       atomval2 (deref counter)
                       [d e f :as vec] (list 1 b 3)]
-                     (list a b d e f vec [@counter atomval1 atomval1 @counter atomval2]))
+                     (list a b d e f vec [atomval2 @counter atomval1 atomval1 @counter atomval2]))
            '(12 81 1 81 3 [1 81 3]
-                [0 1 1 1 1])
+                [0 0 1 1 1 0])
            ))
       )))
