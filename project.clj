90f82ca7c0c839cd9a41e343cd59bc9d74cb7f18
Robert Sanders <robert@zeevex.com>
2012-05-10 03:59:06 -0400

    bump lein-swank version

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[lein-pprint "1.1.1"]
-            [lein-swank "1.4.2"]]  
+            [lein-swank "1.4.4"]]  
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/tools.macro "0.1.1"]])
