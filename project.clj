9e5ffe85b497fe6dbd3dd809fea3b95857eef96f
Robert Sanders <robert@zeevex.com>
2013-11-01 21:44:15 -0400

    upgraded to clojure 1.5

--- project.clj
+++ project.clj
@@ -6,5 +6,5 @@
   :plugins [[lein-pprint "1.1.1"]
             [lein-swank "1.4.4"]]
   :dev-dependencies [[lein-marginalia "0.7.0"]]
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
                  [org.clojure/tools.macro "0.1.1"]])
